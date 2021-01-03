06f7c799fa7adf68f43cce168b97ccacb39cf7a0
島村彩 <ayashimamura@shimamuraayanoMacBook-Air.local>
2021-01-03 16:48:39 +0900

    add

--- app/Models/update.php
+++ app/Models/update.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class update extends Model
 {
-    use HasFactory;
+    protected $table = 'updates';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 }
